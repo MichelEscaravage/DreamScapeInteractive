894eced1ff5ce46e7b3c75915d1e5f62c535db40
Michel Escaravage <108881977+MichelEscaravage@users.noreply.github.com>
2025-02-18 08:37:24 +0100

    Add project files.

---
+++ DreamScapeInteractive/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DreamScapeInteractive.Data.Classes;
+using Windows.Devices.PointOfService.Provider;
+
+namespace DreamScapeInteractive
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;" +
+                "port=3306;" +
+                "user=root;" +
+                "password=;" +
+                "database=csd_gamebib",
+                ServerVersion.Parse("10.4.17-mariadb"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+
+        }
+    }
+
+    
+}
+
+
---
+++ DreamScapeInteractive/Data/Classes/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamScapeInteractive.Data.Classes
+{
+    internal class User
+    {
+        public ObservableCollection<UserItem> Inventory { get; set; } = new ObservableCollection<UserItem>();
+    }
+}
