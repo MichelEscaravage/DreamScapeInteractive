bc8dd8aa748bbaf4ca32b5699d84943522cf18a5
Michel Escaravage <108881977+MichelEscaravage@users.noreply.github.com>
2025-02-19 10:08:55 +0100

    Made 2 filter functions

--- DreamScapeInteractive/CatalogusPage.xaml.cs
+++ DreamScapeInteractive/CatalogusPage.xaml.cs
@@ -1,3 +1,5 @@
+using DreamScapeInteractive.Data.Classes;
+using DreamScapeInteractive.Utility;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -25,20 +27,142 @@ namespace DreamScapeInteractive
     public sealed partial class CatalogusPage : Page
     {
         private readonly AppDbContext _context = new AppDbContext();
+        private FilterState _currentFilters = new FilterState();
 
         public CatalogusPage()
         {
             this.InitializeComponent();
             LoadItems();
         }
+        private void ItemCatalogusGrid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ItemCatalogusScrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+        }
 
+        private void ItemCatalogusGrid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ItemCatalogusScrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+        }
         public void LoadItems()
         {
            var items = _context.Items
                 .Include(i => i.MagicProperty)
+                .Include(i => i.Type)
                 .ToList();
 
+            var magicProperties = items
+                .Where(i => i.MagicProperty != null)
+                .Select(i => i.MagicProperty)
+                .Distinct()
+                .ToList();
+                
             ItemCatalogusGrid.ItemsSource = items;
+            FilterBox.ItemsSource = magicProperties;
+            FilterBox.DisplayMemberPath = "Name";
         }
+
+        public void LoadFilteredItems(FilterState filters)
+        {
+            var filteredItems = _context.Items.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filters.MagicProperty))
+            {
+                filteredItems = filteredItems.Where(i => i.MagicProperty.Name == filters.MagicProperty);
+            }
+
+            if (!string.IsNullOrEmpty(filters.ItemNameOrType))
+            {
+                filteredItems = filteredItems.Where(i => 
+                i.Name.Contains(filters.ItemNameOrType) || 
+                i.Type.Name.Contains(filters.ItemNameOrType));
+            }
+
+            if (filters.Rarity.HasValue)
+            {
+                filteredItems = filteredItems.Where(i => i.Rarity >= filters.Rarity);
+            }
+
+            if (filters.Power.HasValue)
+            {
+                filteredItems = filteredItems.Where(i => i.Power >= filters.Power);
+            }
+
+            if (filters.Speed.HasValue)
+            {
+                filteredItems = filteredItems.Where(i => i.Speed >= filters.Speed);
+            }
+
+            if (filters.Durability.HasValue)
+            {
+                filteredItems = filteredItems.Where(i => i.Durability >= filters.Durability);
+            }
+
+            ItemCatalogusGrid.ItemsSource = filteredItems.ToList();
+        }
+
+        private void SearchItemsBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string filter = SearchItemsBox.Text;
+            _currentFilters.ItemNameOrType = filter;
+            LoadFilteredItems(_currentFilters);
+        }
+
+        private void FilterBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listbox = (ListBox)sender;
+            var selectedItems = listbox.SelectedItems.Cast<Magic_Property>().ToList();
+
+            if (selectedItems.Any())
+            {
+                var selectedMagicProperty = selectedItems.First();
+                _currentFilters.MagicProperty = selectedMagicProperty.Name;
+            }
+            else
+            {
+                _currentFilters.MagicProperty = null; 
+            }
+            LoadFilteredItems(_currentFilters);
+        }
+
+
+        private void FilterSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            var slider = (Slider)sender;
+            var value = e.NewValue;
+
+            if (slider.Name.Contains("Rarity"))
+            {
+                _currentFilters.Rarity = value;
+            }
+            else if (slider.Name.Contains("Power"))
+            {
+                _currentFilters.Power = value;
+            }
+            else if (slider.Name.Contains("Speed"))
+            {
+                _currentFilters.Speed = value;
+            }
+            else if (slider.Name.Contains("Durability"))
+            {
+                _currentFilters.Durability = value;
+            }
+
+            LoadFilteredItems(_currentFilters);
+        }
+
+        private void ResetFiltersButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentFilters = new FilterState();
+            FilterBox.SelectedItem = null;
+
+            DurabilitySlider.Value = 0;
+            SpeedSlider.Value = 0;
+            PowerSlider.Value = 0;
+            RaritySlider.Value = 0;
+
+            LoadItems();
+        }
+
+    
     }
 }
---
+++ DreamScapeInteractive/Utility/FilterState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamScapeInteractive.Utility
+{
+    public class FilterState
+    {
+        public string? MagicProperty { get; set; }
+        public string? ItemNameOrType { get; set; }
+        public double? Rarity { get; set; }
+        public double? Power { get; set; }
+        public double? Speed { get; set; }
+        public double? Durability { get; set; }
+    }
+}
